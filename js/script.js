a0dcc86ea62afebdeaaf6b6e75c14700a37e1e27
NdegwaG <ndegwa.skygo@gmail.com>
2021-06-23 12:07:56 +0300

    changes to JS file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+
+var no1 = parseInt(prompt("Enter a number:"));
+var no2 = parseInt(prompt("Enter another number:"));
+
 var add = function(no1, no2) {
   return no1 + no2;
 };
-var no1 = parseInt(prompt("Enter a number:"));
-var no2 = parseInt(prompt("Enter another number:"));
 
 var subtract = (no1,no2) => no1+no2;
 var divide = (no1,no2) => no1/no2;
